c3aef335218c4a7765433cfb2461642c6d4d09ba
Ribamar Santos <ribamaar@gmail.com>
2018-06-08 22:06:00 -0300

    Adding semantic ui react

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 // create a component
 const Header = () => {
     return (
-        <div style={styles.container}>
+        <div>
             <h1> Header </h1>
         </div>
     );
